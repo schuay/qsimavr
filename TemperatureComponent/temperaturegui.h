375ad8bd1c843e92a0e6d697a28dc5b331c08e9c
Jakob Gruber <jakob.gruber@gmail.com>
2012-08-22 17:10:10 +0200

    Add empty temperature component files

---
+++ TemperatureComponent/temperaturegui.h
@@ -0,0 +1,41 @@
+/* *************************************************************************
+ *  Copyright 2012 Jakob Gruber                                            *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ************************************************************************* */
+
+
+
+#ifndef TEMPERATUREGUI_H
+#define TEMPERATUREGUI_H
+
+#include <QWidget>
+
+namespace Ui {
+class TemperatureGui;
+}
+
+class TemperatureGui : public QWidget
+{
+    Q_OBJECT
+    
+public:
+    explicit TemperatureGui(QWidget *parent = 0);
+    ~TemperatureGui();
+    
+private:
+    Ui::TemperatureGui *ui;
+};
+
+#endif // TEMPERATUREGUI_H
